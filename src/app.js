a744e702ab456ed21e4d61fb66d2e742e3ea5804
mesut <mesut.sezer@student.vinci.be>
2024-11-10 18:50:41 +0100

    converting digital time to berlin time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
-export class Main{
-    
+export class BerlinClock{
+    convertTimeStamp() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        return this.convertBerlinClock(hours,minutes,seconds);
+    }
+
+    convertBerlinClock(hours, minutes, seconds) {
+        return {
+            secondsLight: this.getSecondsLight(seconds),
+            fiveHourBlocks: this.getFiveHourBlocks(hours),
+            simpleHours: this.getSimpleHours(hours),
+            fiveMinuteBlocks: this.getFiveMinuteBlocks(minutes),
+            simpleMinutes: this.getSimpleMinutes(minutes)
+        };
+    }
 }
