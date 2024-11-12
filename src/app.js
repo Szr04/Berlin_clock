9e031d746baf172b48400d325020eaa8fd5beea8
Helalouch Aymane <aymane.helalouch@student.vinci.be>
2024-11-12 21:02:21 +0100

    add test + refactor : should return YXXX when given 6 minutes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 export class BerlinClock {
     berlinClockSingleMinute(minutes) {
         if (minutes === 0 || minutes === 5) return "XXXX";
-        if (minutes === 1) return "YXXX";
+        if (this.hasAnOffsetOf1(minutes)) return "YXXX";
         if (minutes === 2) return "YYXX";
         if (minutes === 3) return "YYYX";
         if (minutes === 4) return "YYYY";
         
         return "XXXX"; 
     }
+
+    hasAnOffsetOf1(minutes) {
+        return minutes % 5 === 1;
+    }
+
+
 }
