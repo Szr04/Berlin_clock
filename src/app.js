e56118c81fc28ab6c59504ff5e1d8e1257593461
Helalouch Aymane <aymane.helalouch@student.vinci.be>
2024-11-12 20:41:25 +0100

    add test : should return "YYYX" when given 3 minutes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ export class BerlinClock {
         if (minutes === 0) return "XXXX";
         if (minutes === 1) return "YXXX";
         if (minutes === 2) return "YYXX";
+        if (minutes === 3) return "YYYX";
         
         return "XXXX"; 
     }
