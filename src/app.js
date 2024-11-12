7ca7dacfec7a2c2b904ccfd6736c2cb240a17603
Helalouch Aymane <aymane.helalouch@student.vinci.be>
2024-11-12 20:47:40 +0100

    add test : should return XXXX when given 5 minutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export class BerlinClock {
     berlinClockSingleMinute(minutes) {
-        if (minutes === 0) return "XXXX";
+        if (minutes === 0 || minutes === 5) return "XXXX";
         if (minutes === 1) return "YXXX";
         if (minutes === 2) return "YYXX";
         if (minutes === 3) return "YYYX";
