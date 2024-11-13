0cbc878bc57d65579d696a75efa6877d0254886f
mesut <mesut.sezer@student.vinci.be>
2024-11-13 22:04:34 +0100

    add  : should return R when given 10 seconds

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -735,6 +735,15 @@ describe("BerlinClockSeconds function should return the seconds line", function 
 
         expect(result).toBe("X");
     });
+
+    it("should return R when given 10 seconds", function(){
+        const date = new Date();
+        date.setSeconds(10);
+
+        const result = clock.berlinClockSeconds(date.getSeconds());
+
+        expect(result).toBe("R");
+    });
 })    
     
 
